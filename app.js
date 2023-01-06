02d4af0b3ac92c7ad615a81c01f5eabe741c42b4
solo5star <solo_5@naver.com>
2023-01-06 08:11:52 +0000

    app.js 수정

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const { Server } = require("socket.io");
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { Server } = require('socket.io');
 
-const db = require("./db");
-const jwt = require("./middlewares/jwt");
-const api = require("./routes/api");
-const auth = require("./routes/auth");
-const diagnostics = require("./routes/diagnostics");
-const ChatService = require("./services/chat-service");
+const db = require('./db');
+const jwt = require('./middlewares/jwt');
+const errorHandler = require('./middlewares/error-handler');
+const ChatService = require('./services/chat-service');
+const routes = require('./routes');
 
 // basic dependencies
 const app = express();
@@ -19,18 +18,15 @@ const httpServer = http.createServer(app);
 ChatService.init(new Server(httpServer));
 
 // middlewares
-app.use(express.static("static"));
+app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 app.use(jwt);
-
-// routes
-app.use("/api/auth", auth);
-app.use("/api", api);
-app.use("/api", diagnostics);
+app.use(routes);
+app.use(errorHandler);
 
 db.connect().then(() => {
-  httpServer.listen(8000, () => console.log("Server listening on port 8000"));
+  httpServer.listen(8000, () => console.log('Server listening on port 8000'));
 });
